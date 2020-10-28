542f99c55be5d8b02adbe2e4aad6a6c22906d064
kezhongfa <zhongfa.ke@shuyun.com>
2020-10-28 22:40:10 +0800

    feat: 修改用户model 部分字段为话题引用

--- src/models/users.ts
+++ src/models/users.ts
@@ -11,13 +11,13 @@ export const userSchema = createSchema({
   avatar_url: { type: String },
   gender: { type: String, enum: ['male', 'female'], default: 'male' },
   headline: { type: String },
-  locations: { type: [{ type: String }], select: false },
-  business: { type: String, select: false },
+  locations: { type: [{ type: Schema.Types.ObjectId, ref: 'Topic' }], select: false },
+  business: { type: Schema.Types.ObjectId, ref: 'Topic', select: false },
   employments: {
     type: [
       {
-        company: { type: String },
-        job: { type: String },
+        company: { type: Schema.Types.ObjectId, ref: 'Topic' },
+        job: { type: Schema.Types.ObjectId, ref: 'Topic' },
       },
     ],
     select: false,
@@ -25,8 +25,8 @@ export const userSchema = createSchema({
   educations: {
     type: [
       {
-        school: { type: String },
-        major: { type: String },
+        school: { type: Schema.Types.ObjectId, ref: 'Topic' },
+        major: { type: Schema.Types.ObjectId, ref: 'Topic' },
         diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
         entrance_year: { type: Number },
         graduation_year: { type: Number },
